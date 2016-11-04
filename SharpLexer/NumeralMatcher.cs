31d7a24ae5304d452376588c637f7175c68d06bf
xubo <sunicdavy@qq.com>
2016-11-04 16:50:49 +0800

    增加正数解析

--- SharpLexer/NumeralMatcher.cs
+++ SharpLexer/NumeralMatcher.cs
@@ -31,4 +31,34 @@ namespace SharpLexer
         }
 
     }
+
+    public class PositiveNumeralMatcher : Matcher
+    {
+        public PositiveNumeralMatcher(object id)
+        {
+            _id = (int)id;
+        }
+
+        public override Token Match(Tokenizer tz)
+        {
+
+            if (!Char.IsDigit(tz.Current)  )
+                return null;
+
+            var pos = tz.Pos;
+
+            int beginIndex = tz.Index;
+
+
+            do
+            {
+                tz.Consume();
+
+            } while (char.IsDigit(tz.Current) || tz.Current == '.');
+
+
+            return new Token(pos, this, tz.Source.Substring(beginIndex, tz.Index - beginIndex));
+        }
+
+    }
 }
